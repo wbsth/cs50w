c3cfc6c8354696ffbf9c4373b86fa56b3f3d182b
Michal Ungeheuer <wbsth@users.noreply.github.com>
2020-09-09 19:48:12 +0200

    posting new auction requires being logged

--- project2/auctions/views.py
+++ project2/auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 from .forms import NewAuctionForm
@@ -64,6 +65,7 @@ def register(request):
         return render(request, "auctions/register.html")
 
 
+@login_required
 def new_auction(request):
     if request.method == "POST":
         form = NewAuctionForm(request.POST)
--- project2/commerce/settings.py
+++ project2/commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+# from django.urls import reverse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'login'
