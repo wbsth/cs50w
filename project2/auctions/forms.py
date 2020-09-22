b36cde1c28fc79dfff658043a5e2a07e01261959
Michal Ungeheuer <wbsth@users.noreply.github.com>
2020-09-22 23:11:06 +0200

    new auction form finished using materialize-css-form library

--- project2/auctions/forms.py
+++ project2/auctions/forms.py
@@ -5,5 +5,5 @@ from auctions.models import AuctionListening, Category
 class NewAuctionForm(forms.ModelForm):
     class Meta:
         model = AuctionListening
-        fields = ['title', 'description', 'image_url', 'starting_bid']
+        fields = ['title', 'description', 'category', 'image_url', 'starting_bid']
 
